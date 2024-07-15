194dda9f3de8e5f9db36927578e9114f1a39474e
MoonLeeeaf <150461955+MoonLeeeaf@users.noreply.github.com>
2024-07-15 23:43:44 +0800

    是小更新耶
    package.json: 修改 scripts
    index.js: 挪到根目录了, 并修复了无法识别 --build 的问题

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const child_process = require('node:child_process')
-const logger = require('./logger')
-const io = require('./io')
-const { lingChairConfig } = require('./config')
+const logger = require('./src/logger')
+const io = require('./src/io')
+const { lingChairConfig } = require('./src/config')
 
 function fork(a) {
     let c = child_process.fork(a, {
@@ -14,13 +14,11 @@ function fork(a) {
     return c
 }
 
-console.log(process.execArgv)
-if (process.execArgv.length != 0 && process.execArgv[0] == "--build")
+if (process.argv.includes("--build"))
     fork('./src/builder.js').once('exit', (code) => {
         if (code != 0) {
             logger.error(`构建网页失败! 请查看日志寻找原因, 若多次无法解决请寻找他人解决`)
             process.exit(1)
-            return
         }
         fork('./src/starter.js')
     })
